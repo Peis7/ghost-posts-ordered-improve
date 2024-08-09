86487e73cdc84c53438a97fdba9d4f940637c93a
Peis7 <ing.cabrera.acosta@gmail.com>
2024-08-08 21:23:47 -0600

    cover test for posts provider

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -6,25 +6,29 @@ import { HttpService } from '@nestjs/axios';
 import * as path from 'path';
 import configuration from '../config/configuration';
 import { of } from 'rxjs';
+import { Post } from '../interfaces/post';
 
 
-describe('Posts', () => {
+describe('Posts Service', () => {
   let service: PostsService;
   let mockHttpService: { get: jest.Mock };
   const ENV = process.env.NODE_ENV;
+  let mockPosts = [];
 
-  console.log(ENV);
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     mockHttpService = {
       get: jest.fn(),
     };
 
-    // Define mock posts data
-    const mockPosts = [
+    mockPosts = [
       { title: 'Post 1', url: 'url1', featured: true, published_at: new Date('1990-02-20T20:11:10.230Z'), 
           tags: [{ name: 'index-1' }, { name: 'no_menu' }] },
       { title: 'Post 2', url: 'url2', featured: false, published_at: new Date('1960-06-29T20:11:10.230Z'), 
           tags: [{ name: 'index-100' }] },
+          { title: 'Post 3', url: 'url3', featured: false, published_at: new Date('1958-06-29T20:11:10.230Z'), 
+            tags: [{ name: 'index-50' }] },
     ];
 
     mockHttpService.get.mockReturnValue(of({ data: { posts: mockPosts } }));
@@ -62,11 +66,21 @@ describe('Posts', () => {
   });
 
   it('should return an array of posts', async () => {
-    const fields = ['title', 'url', 'featured', 'published_at'];
-    const include = ['tags'];
-    const result = await service.get(fields, include);
+    const isNewSpy = jest.spyOn(service as any, 'isNew');
+    const getFirstTagWithPattherSpy = jest.spyOn(service as any, 'getFirstTagWithPatther');
 
-    console.log(result);
+
+    const posts = await service.get([], []);
+
+    expect(isNewSpy).toHaveBeenCalledTimes(posts.length);
+    expect(getFirstTagWithPattherSpy).toHaveBeenCalledTimes(posts.length*2);
+
+  });
+
+  it('should return an array of posts', async () => {
+    const spyGet = jest.spyOn(service, 'get');
+    const result = await service.get(['some'], ['value']);
+
     expect(result).toEqual([
       {
           index: 1,
@@ -86,9 +100,18 @@ describe('Posts', () => {
         url: 'url2',
         featured: false,
         new: false
+      },
+      {
+        index: 50,
+        title: 'Post 3',
+        level: null,
+        no_menu: false,
+        url: 'url3',
+        featured: false,
+        new: false
       }
 ,
     ]);
-    //expect(service.get).toHaveBeenCalledWith( fields, include );
+    expect(spyGet).toHaveBeenCalledWith( ['some'], ['value'] );
   });
 });
